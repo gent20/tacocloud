63bf1ec525f8e4074248129e9b77fdc57a2a9dab
Gent Kastrati <gentkastratio@gmail.com>
2020-11-15 12:57:41 +0100

    orderForm view , created a designtacoController , added some validation

--- src/main/java/spring/tacocloud/controller/DesignTacoController.java
+++ src/main/java/spring/tacocloud/controller/DesignTacoController.java
@@ -43,6 +43,14 @@ public class DesignTacoController {
         return "design";
     }
 
+    @PostMapping
+    public String processDesign(Design design) {
+    // Save the taco design...
+    // We'll do this in chapter 3
+        log.info("Processing design: " + design);
+        return "redirect:/orders/current";
+    }
+
 
     private List<Ingredient> filterByType(List<Ingredient> ingredients, Ingredient.Type type) {
         return ingredients
