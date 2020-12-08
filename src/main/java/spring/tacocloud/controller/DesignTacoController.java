a59d9e01a2b694d37042df03336d5738b055ebab
Gent Kastrati <gentkastratio@gmail.com>
2020-12-08 16:57:16 +0100

    Converted methods on designtacocontroller and tacorepo to reactive

--- src/main/java/spring/tacocloud/controller/DesignTacoController.java
+++ src/main/java/spring/tacocloud/controller/DesignTacoController.java
@@ -1,13 +1,11 @@
 package spring.tacocloud.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.hateoas.server.EntityLinks;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import spring.tacocloud.model.Taco;
 import spring.tacocloud.repository.TacoRepository;
 
@@ -27,9 +25,18 @@ public class DesignTacoController {
     }
 
     @GetMapping("/recent")
-    public Iterable<Taco> recentTacos() {
-        PageRequest page = PageRequest.of(
-                0, 12, Sort.by("createdAt").descending());
-        return tacoRepo.findAll(page).getContent();
+    public Flux<Taco> recentTacos() {
+        return tacoRepo.findAll().take(12);
+    }
+
+    @GetMapping("/{id}")
+    public Mono<Taco> tacoById(@PathVariable("id") Long id) {
+        return tacoRepo.findById(id);
+    }
+
+    @PostMapping(consumes="application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<Taco> postTaco(@RequestBody Mono<Taco> tacoMono) {
+        return tacoRepo.saveAll(tacoMono).next();
     }
 }
--- src/main/java/spring/tacocloud/repository/TacoRepository.java
+++ src/main/java/spring/tacocloud/repository/TacoRepository.java
@@ -1,8 +1,8 @@
 package spring.tacocloud.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import spring.tacocloud.model.Taco;
 
-public interface TacoRepository extends CrudRepository<Taco,Long> {
-
+public interface TacoRepository
+        extends ReactiveCrudRepository<Taco, Long> {
 }
