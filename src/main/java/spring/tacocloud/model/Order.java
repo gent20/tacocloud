abeba1abda3afa78643c073ecaf0f2b5c4259b54
Gent Kastrati <gentkastratio@gmail.com>
2020-12-09 20:03:50 +0100

    Created UDTs

--- src/main/java/spring/tacocloud/model/Order.java
+++ src/main/java/spring/tacocloud/model/Order.java
@@ -1,69 +1,34 @@
 package spring.tacocloud.model;
 
+import com.datastax.oss.driver.api.core.uuid.Uuids;
 import lombok.Data;
-import org.hibernate.validator.constraints.CreditCardNumber;
+import org.springframework.data.cassandra.core.mapping.Column;
+import org.springframework.data.cassandra.core.mapping.PrimaryKey;
+import org.springframework.data.cassandra.core.mapping.Table;
 
-import javax.persistence.*;
-import javax.validation.constraints.Digits;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
+import java.util.UUID;
 
 @Data
-@Entity
-@Table(name="Taco_Order")
+@Table("tacoorders")
 public class Order implements Serializable {
-
     private static final long serialVersionUID = 1L;
 
-    @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
-    private Long id;
+    @PrimaryKey
 
-    private Date placedAt;
+    private UUID id = Uuids.timeBased();
 
-    @ManyToOne
-    private User user;
+    private Date placedAt = new Date();
 
-    @NotBlank(message="Delivery name is required")
-    private String deliveryName;
+    @Column("user")
+    private UserUDT user;
+    // delivery and credit card properties omitted for brevity's sake
 
-    @NotBlank(message="Street is required")
-    private String deliveryStreet;
-
-    @NotBlank(message="City is required")
-    private String deliveryCity;
-
-    @NotBlank(message="State is required")
-    private String deliveryState;
-
-    @NotBlank(message="Zip code is required")
-    private String deliveryZip;
-
-    @CreditCardNumber(message="Not a valid credit card number")
-    private String ccNumber;
-
-    @Pattern(regexp="^(0[1-9]|1[0-2])([\\/])([1-9][0-9])$",
-            message="Must be formatted MM/YY")
-    private String ccExpiration;
-
-    @Digits(integer=3, fraction=0, message="Invalid CVV")
-    private String ccCVV;
-
-
-    @ManyToMany(targetEntity=Taco.class)
-    private List<Taco> tacos = new ArrayList<>();
-
-    public void addDesign(Taco design) {
+    @Column("tacos")
+    private List<TacoUDT> tacos = new ArrayList<>();
+    public void addDesign(TacoUTD design) {
         this.tacos.add(design);
     }
-
-    @PrePersist
-    void placedAt() {
-        this.placedAt = new Date();
-    }
-
 }
